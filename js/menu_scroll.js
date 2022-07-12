e1e292a7f1e726343a3a7e55e43c5ffc681129ce
somang1206 <dlthakd1206@naver.com>
2022-07-12 11:42:47 +0900

    header

--- js/menu_scroll.js
+++ js/menu_scroll.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
 $(".scroll").click(function(event){
 
-event.preventDefault();
+//event.preventDefault();
 
 $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 
